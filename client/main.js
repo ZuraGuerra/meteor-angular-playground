92b07871af3047d6021b22306d6530c43c645317
Zura Guerra <guerrazuramx@gmail.com>
2016-07-20 15:06:13 -0500

    :bird: Add dynamic periquis to the view

--- client/main.js
+++ client/main.js
@@ -3,4 +3,16 @@ import angularMeteor from 'angular-meteor';
 
 angular.module('aviary', [
   angularMeteor
-]);
+])
+  .controller('PeriquisController', function($scope) {
+    'ngInject';
+    
+    $scope.periquis =
+    [
+      {
+        'name': 'Milorad',
+        'color': 'green',
+        'favoriteFruit': 'apple'
+      }
+    ]
+  });
